1a8451718545f799b2a3d69764e6175b6b74ea02
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-04-05 12:29:05 +0530

    done with assignment people of GOT

---
+++ People of GOT with Reduce/index.js
@@ -0,0 +1,81 @@
+// NOTE: You can only use the (reduce) array method to solve this exercise:
+
+// Testing your result after writing your function
+
+function countAllPeople() {
+  const totalPeople = got.houses.reduce((acc, house) => {
+    return acc + house.people.length;
+  }, 0);
+  return totalPeople;
+}
+
+// Output should be 33
+
+function peopleByHouses() {
+  const final = got.houses.reduce((acc, house) => {
+    acc[house.name] = house.people.length;
+    return acc;
+  }, {});
+  return final;
+}
+
+// Output should be
+//{Arryns: 1, Baratheons: 6, Dothrakis: 1, Freys: 1, Greyjoys: 3, Lannisters: 4,Redwyne: 1,Starks: 8,Targaryens: 2,Tullys: 4,Tyrells: 2}
+
+function everyone() {
+  const final = got.houses.reduce((acc, house) => {
+    names = house.people.map((person) => person.name);
+    return acc.concat(names);
+  }, []);
+  return final;
+}
+
+// Output should be
+//["Eddard "Ned" Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon "Bran" Stark", "Rickon Stark", "Jon Snow", "Tywin Lannister", "Tyrion Lannister", "Jaime Lannister", "Queen Cersei (Lannister) Baratheon", "King Robert Baratheon", "Stannis Baratheon", "Renly Baratheon", "Joffrey Baratheon", "Tommen Baratheon", "Myrcella Baratheon", "Daenerys Targaryen", "Viserys Targaryen", "Balon Greyjoy", "Theon Greyjoy", "Yara Greyjoy", "Margaery (Tyrell) Baratheon", "Loras Tyrell", "Catelyn (Tully) Stark", "Lysa (Tully) Arryn", "Edmure Tully", "Brynden Tully", "Olenna (Redwyne) Tyrell", "Walder Frey", "Jon Arryn", "Khal Drogo"]
+
+function nameWithS() {
+  let peopleWithS = everyone();
+
+  peopleWithS = peopleWithS.filter((name) => name.toLowerCase().includes("s"));
+
+  return peopleWithS;
+}
+
+// Output should be
+// ["Eddard "Ned" Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon "Bran" Stark", "Rickon Stark", "Jon Snow", "Tywin Lannister", "Tyrion Lannister", "Jaime Lannister", "Queen Cersei (Lannister) Baratheon", "Stannis Baratheon", "Daenerys Targaryen", "Viserys Targaryen", "Loras Tyrell", "Catelyn (Tully) Stark", "Lysa (Tully) Arryn"]
+
+function nameWithA() {
+  let peopleWithA = everyone();
+  peopleWithA = peopleWithA.filter((name) => name.toLowerCase().includes("a"));
+  return peopleWithA;
+}
+
+// Output should be
+// ["Eddard Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon Stark", "Rickon Stark", "Tywin Lannister", "Tyrion Lannister", "Jaime Lannister", "Cersei Baratheon", "Robert Baratheon", "Stannis Baratheon", "Renly Baratheon", "Joffrey Baratheon", "Tommen Baratheon", "Myrcella Baratheon", "Daenerys Targaryen", "Viserys Targaryen", "Balon Greyjoy", "Yara Greyjoy", "Margaery Baratheon", "Loras Tyrell", "Catelyn Stark", "Lysa Arryn", "Olenna Tyrell", "Walder Frey", "Jon Arryn", "Khal Drogo"]
+
+function surnameWithS() {
+  let surname = everyone();
+  return surname.filter((h) => h.split(" ")[1].startsWith("S"));
+}
+
+// Output should be
+// ["Eddard Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon Stark", "Rickon Stark", "Jon Snow", "Catelyn Stark"]
+
+function surnameWithA() {
+  let surname = everyone();
+  return surname.filter((h) => h.split(" ")[1].startsWith("A"));
+}
+
+// Output should be
+// ["Lysa Arryn", "Jon Arryn"]
+
+function peopleNameOfAllHouses() {
+  let final = got.houses.reduce((acc, cv) => {
+    acc[cv.name] = cv.people.map((h) => h.name);
+    return acc;
+  }, {});
+  return final;
+}
+
+// Output should be
+// {Arryns: ["Jon Arryn"], Baratheons: ["Robert Baratheon", "Stannis Baratheon", "Renly Baratheon", "Joffrey Baratheon", "Tommen Baratheon", "Myrcella Baratheon"], Dothrakis: ["Khal Drogo"], Freys: ["Walder Frey"], Greyjoys: ["Balon Greyjoy", "Theon Greyjoy", "Yara Greyjoy"], Lannisters: ["Tywin Lannister", "Tyrion Lannister", "Jaime Lannister", "Cersei Baratheon"], Redwyne: ["Olenna Tyrell"], Starks: ["Eddard Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon Stark", "Rickon Stark", "Jon Snow"], Targaryens: ["Daenerys Targaryen", "Viserys Targaryen"], Tullys: ["Catelyn Stark", "Lysa Arryn", "Edmure Tully", "Brynden Tully"], Tyrells: ["Margaery Baratheon", "Loras Tyrell"]}
